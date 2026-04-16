2de4d50c1ad4e35f62911948c2472981fa4cc210
kazanefu <kazanefu12@gmail.com>
2026-04-16 18:36:29 +0900

    add bevy icon

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0c905c29.js',
+                path_to_root + 'searchindex-f044e6fc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
